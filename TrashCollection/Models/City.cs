4e739684a2e0c881fad1b57be0d86fae57bb10a6
DamonYoung42 <damonyoung@wi.rr.com>
2016-09-13 16:12:39 -0500

    initial commit

---
+++ TrashCollection/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrashCollection.Models
+{
+    public class City
+    {
+        public City()
+        {
+
+        }
+        [Key]
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+
+        [ForeignKey("State")]
+        public string StateID { get; set; }
+        public State State { get; set; }
+    }   
+}
