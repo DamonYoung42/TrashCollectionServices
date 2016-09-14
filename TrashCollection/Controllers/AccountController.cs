df53f5c2981c9f9ebbae13d2b214dc11c9cf7e81
DamonYoung42 <damonyoung@wi.rr.com>
2016-09-14 16:43:02 -0500

    customerscontroller file added.

--- TrashCollection/Controllers/AccountController.cs
+++ TrashCollection/Controllers/AccountController.cs
@@ -167,6 +167,10 @@ namespace TrashCollection.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
                     await this.UserManager.AddToRoleAsync(user.Id, model.UserRole);
+                    //if (model.UserRole == "Customer")
+                    //{
+                    //    return RedirectToAction("Create", "Customer");
+                    //}
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
