aed03a35d62c7c465d32c0a3e1263dfe21b63856
rvishnub <rvishnub@yahoo.com>
2016-09-15 07:55:27 -0500

    no real changes

--- TrashCollection/Classes/Employee.cs
+++ TrashCollection/Classes/Employee.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TrashCollection.Classes
-{
-    public class Employee
-    {
-        public int employeeID;
-        public string firstName;
-        public string lastName;
-        public string emailAddress;
-        public string role; //employee or customer
-        public int passcode; //four digit, combined with empID
-        public string password;
-        public Pickup pickup;
-        public Dictionary<int, Pickup> employeePickups;
+//namespace TrashCollection.Classes
+//{
+//    public class Employee
+//    {
+//        public int employeeID;
+//        public string firstName;
+//        public string lastName;
+//        public string emailAddress;
+//        public string role; //employee or customer
+//        public int passcode; //four digit, combined with empID
+//        public string password;
+//        public Pickup pickup;
+//        public Dictionary<int, Pickup> employeePickups;
 
-        public Employee()
-        {
+//        public Employee()
+//        {
             
-        }
+//        }
 
-        //}
-        //ViewRoute
-        //ViewCalendar
-        //ViewPickupInformation(address, pickup schedule)
-        ////employee does not need to know what the customer name is
+//        //}
+//        //ViewRoute
+//        //ViewCalendar
+//        //ViewPickupInformation(address, pickup schedule)
+//        ////employee does not need to know what the customer name is
 
 
-    }
-}
+//    }
+//}
