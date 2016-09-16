2da2fb935f6d855873b1544629b994f57b6a3e8d
DamonYoung42 <damonyoung@wi.rr.com>
2016-09-16 17:37:06 -0500

    pickup controller, views created

---
+++ TrashCollection/Controllers/PickupsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollection.Models;
+
+namespace TrashCollection.Controllers
+{
+    public class PickupsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Pickups
+        public ActionResult Index()
+        {
+            var pickup = db.Pickup.Include(p => p.Address).Include(p => p.Employee);
+            return View(pickup.ToList());
+        }
+
+        // GET: Pickups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pickup pickup = db.Pickup.Find(id);
+            if (pickup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pickup);
+        }
+
+        // GET: Pickups/Create
+        public ActionResult Create()
+        {
+            ViewBag.AddressID = new SelectList(db.Address, "AddressID", "Street1");
+            ViewBag.EmployeeID = new SelectList(db.Employee, "EmployeeID", "FirstName");
+            return View();
+        }
+
+        // POST: Pickups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PickupID,PickupDate,Status,EmployeeID,AddressID")] Pickup pickup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pickup.Add(pickup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AddressID = new SelectList(db.Address, "AddressID", "Street1", pickup.AddressID);
+            ViewBag.EmployeeID = new SelectList(db.Employee, "EmployeeID", "FirstName", pickup.EmployeeID);
+            return View(pickup);
+        }
+
+        // GET: Pickups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pickup pickup = db.Pickup.Find(id);
+            if (pickup == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AddressID = new SelectList(db.Address, "AddressID", "Street1", pickup.AddressID);
+            ViewBag.EmployeeID = new SelectList(db.Employee, "EmployeeID", "FirstName", pickup.EmployeeID);
+            return View(pickup);
+        }
+
+        // POST: Pickups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PickupID,PickupDate,Status,EmployeeID,AddressID")] Pickup pickup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pickup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AddressID = new SelectList(db.Address, "AddressID", "Street1", pickup.AddressID);
+            ViewBag.EmployeeID = new SelectList(db.Employee, "EmployeeID", "FirstName", pickup.EmployeeID);
+            return View(pickup);
+        }
+
+        // GET: Pickups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pickup pickup = db.Pickup.Find(id);
+            if (pickup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pickup);
+        }
+
+        // POST: Pickups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pickup pickup = db.Pickup.Find(id);
+            db.Pickup.Remove(pickup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
