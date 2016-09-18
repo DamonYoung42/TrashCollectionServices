69daef21507acb7657b82e0624a105611d105ca9
DamonYoung42 <damonyoung@wi.rr.com>
2016-09-18 12:22:39 -0500

    login fix.

--- TrashCollection/Controllers/AccountController.cs
+++ TrashCollection/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace TrashCollection.Controllers
 
                 var customerId = context.Customer.Where(y => y.UserId == userId).Select(m => m.CustomerID).First();
                 var addressId = context.Address.Where(y => y.CustomerID == customerId).Select(m => m.AddressID).First();
-                return RedirectToAction("Details/" + addressId, "Addresses");
+                return RedirectToAction("Index", "Addresses");
             }
             else if (User.IsInRole("Employee"))
             {
@@ -102,20 +102,19 @@ namespace TrashCollection.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    if (User.IsInRole("Customer"))
-                    {
-                        return RedirectToAction("Index", "Addresses");
-                    }
-                    else
-                    {
+                    //if (User.IsInRole("Customer"))
+                    //{
+                    //    return RedirectToAction("Index", "Addresses");
+                    //}
+                    //else
+                    //{
+                    //    var userId = User.Identity.GetUserId();
+                    //    var employeeId = context.Employee.Where(y => y.UserId == userId).First().EmployeeID;
+                    //    return RedirectToAction("Details/" + employeeId, "Employees");
+                    //}
 
-                        var userId = User.Identity.GetUserId();
-                        var employeeId = context.Employee.Where(y => y.UserId == userId).First().EmployeeID;
-                        return RedirectToAction("Details/" + employeeId, "Employees");
-                    }
-               
 
-                    //return RedirectToLocal(returnUrl);
+                    return RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
